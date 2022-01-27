3286fe71bfd703762ee48f386b06e53fc451886f
CDeweger <caroljydeweger@gmail.com>
2022-01-26 23:50:58 -0800

    update signup page styling

--- client/src/pages/SignUpPage/SignUpPage.js
+++ client/src/pages/SignUpPage/SignUpPage.js
@@ -45,62 +45,75 @@ class SignUpPage extends Component {
         </Helmet>
         <h1 className="signupPage__heading">Sign Up</h1>
         <form className="signup-form" onSubmit={this.signup}>
-          <div className="signup-form__field">
-            <label className="signup-form__label">E-mail/Username</label>
-            <input
-              className="signup-form__input"
-              type="email"
-              name="username"
-            />
+          <div className="signup-form__group">
+            <div className="signup-form__field">
+              <label className="signup-form__label">E-mail/Username</label>
+              <input
+                className="signup-form__input"
+                type="email"
+                name="username"
+              />
+            </div>
+            <div className="signup-form__field">
+              <label className="signup-form__label">Organization Name</label>
+              <input className="signup-form__input" type="text" name="name" />
+            </div>
           </div>
-          <div className="signup-form__field">
-            <label className="signup-form__label">Organization Name</label>
-            <input className="signup-form__input" type="text" name="name" />
+          <div className="signup-form__group">
+            {" "}
+            <div className="signup-form__field">
+              <label className="signup-form__label">Program Type</label>
+              <select name="type" className="signup-form__input">
+                <option disabled selected>
+                  Please select
+                </option>
+                <option>Homeless Shelter</option>
+                <option>Free/Low Cost Food Program</option>
+                <option>First Nation</option>
+              </select>
+            </div>
+            <div className="signup-form__field">
+              <label className="signup-form__label">Location</label>
+              <input
+                className="signup-form__input"
+                type="text"
+                name="location"
+              />
+            </div>
           </div>
-          <div className="signup-form__field">
-            <label className="signup-form__label">Program Type</label>
-            <select name="type" className="signup-form__input">
-              <option disabled selected>
-                Please select
-              </option>
-              <option>Homeless Shelter</option>
-              <option>Free/Low Cost Food Program</option>
-              <option>First Nation</option>
-            </select>
-          </div>
-          <div className="signup-form__field">
-            <label className="signup-form__label">Location</label>
-            <input className="signup-form__input" type="text" name="location" />
-          </div>
-          <div className="signup-form__field">
+          {/* <div className="signup-form__group--website "> */}
+          <div className="signup-form__field signup-form__field--website ">
             <label className="signup-form__label">Website</label>
             <input className="signup-form__input" type="text" name="website" />
           </div>
-          <div className="signup-form__field">
+          {/* </div> */}
+          <div className="signup-form__field signup-form__field--description">
             <label className="signup-form__label">Description</label>
-            <input
-              className="signup-form__input"
+            <textarea
+              className="signup-form__input signup-form__input--description"
               type="text"
               name="description"
             />
           </div>
-          <div className="signup-form__field">
-            <label className="signup-form__label">Password</label>
-            <input
-              className="signup-form__input"
-              type="password"
-              name="password"
-            />
-          </div>
-          <div className="signup-form__field">
-            <label className="signup-form__label" name="confirm-password">
-              Confrim Password
-            </label>
-            <input
-              className="signup-form__input"
-              type="password"
-              name="confrimPassword"
-            />
+          <div className="signup-form__group">
+            <div className="signup-form__field">
+              <label className="signup-form__label">Password</label>
+              <input
+                className="signup-form__input"
+                type="password"
+                name="password"
+              />
+            </div>
+            <div className="signup-form__field">
+              <label className="signup-form__label" name="confirm-password">
+                Confrim Password
+              </label>
+              <input
+                className="signup-form__input"
+                type="password"
+                name="confrimPassword"
+              />
+            </div>
           </div>
           <div className="signup-form__field">
             <button className="signup-form__button">Sign Up</button>
