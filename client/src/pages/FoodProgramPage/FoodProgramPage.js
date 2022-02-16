059eb962178a6965dc55f709726bd9859dbc6f3a
CDeweger <caroljydeweger@gmail.com>
2022-02-16 15:43:40 -0800

    update FoodProgramPage with hooks

--- client/src/pages/FoodProgramPage/FoodProgramPage.js
+++ client/src/pages/FoodProgramPage/FoodProgramPage.js
@@ -1,56 +1,45 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import FoodProgramCard from "../../components/FoodProgramCard/FoodProgramCard";
 import "./FoodProgramPage.scss";
 const API_URL = process.env.REACT_APP_API_URL;
 
-class FoodProgramPage extends Component {
-  state = {
-    foodProgramList: [],
-  };
+const FoodProgramPage = () => {
+  const [foodProgramList, setFoodProgramList] = useState([]);
 
-  getFoodProgramList = () => {
+  const getFoodProgramList = () => {
     axios
       .get(`${API_URL}organization`)
       .then((res) => {
-        this.setState({
-          foodProgramList: res.data.filter(
+        setFoodProgramList(
+          res.data.filter(
             (type) => type.program_type === "Free/Low Cost Food Program"
-          ),
-        });
+          )
+        );
       })
       .catch((_err) => {
         console.log("error");
       });
   };
 
-  componentDidMount() {
-    this.getFoodProgramList();
+  useEffect(() => {
+    getFoodProgramList();
+  }, []);
+
+  if (foodProgramList) {
+    return null;
   }
 
-  render() {
-    console.log(this.state.foodProgramList);
-
-    if (!this.state.foodProgramList) {
-      return null;
-    }
-
-    return (
-      <div className="FirstNationPage">
-        <h1 className="FirstNationPage__heading">
-          Free & Low Cost Food Program
-        </h1>
-        {this.state.foodProgramList.map((foodProgram) => {
-          return (
-            <FoodProgramCard
-              key={foodProgram.id}
-              foodProgramList={foodProgram}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="FirstNationPage">
+      <h1 className="FirstNationPage__heading">Free & Low Cost Food Program</h1>
+      {foodProgramList.map((foodProgram) => {
+        return (
+          <FoodProgramCard key={foodProgram.id} foodProgramList={foodProgram} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default FoodProgramPage;
